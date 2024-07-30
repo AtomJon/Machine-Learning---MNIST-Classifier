7984cda9e98a45822ec47d77ab94ed00965fd46d
Neutronic John <jonbruskin@gmail.com>
2024-07-30 16:37:38 +0200

    Describe relu function

--- activation_functions.py
+++ activation_functions.py
@@ -2,6 +2,7 @@ from torch import Tensor
 import torch
 import torch.nn.functional as F
 
+# ReLu activation function. Disables all values in an arbitrary tensor that are less than 0
 def relu(x: Tensor):
     # If a given input is larger than 0, return x otherwise return 0.
     return torch.where(x > 0, x, 0.)
